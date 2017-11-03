f8df6aa9ae976ea11e10f31c104a469e3e2e921a
Simon Baird <sbaird@redhat.com>
2017-11-03 13:58:15 +1000

    Add LICENSE file
    I picked GPL v2, same as rpmdelint.

--- gerrit-nag.py
+++ gerrit-nag.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
 from typing import AnyStr, Iterable, List, TypeVar, Dict
 import argparse
 import requests
